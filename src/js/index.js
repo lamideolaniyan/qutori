607dd6f70bd1863d6b476e41114bbc368be5859a
cyber-mat <olaniyanolamide@gmail.com>
2020-10-04 14:32:42 +0100

    successful integrating of flapickr.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,30 @@
-// import flatpickr from 'flatpickr';
+import flatpickr from 'flatpickr';
+import '../../node_modules/flatpickr/dist/flatpickr.min.css';
 
-// const inputArray = Array.from(document.querySelectorAll('input'));
-// const fp = flatpickr('#testing');
+const inputArray = Array.from(document.querySelectorAll('input'));
+const fp = () => {
+  const today = new Date();
 
-// window.addEventListener('load', fp);
+  flatpickr('#booking-date', {
+    altInput: true,
+    altFormat: 'F j, Y',
+    dateFormat: 'Y-m-d',
+    defaultDate: today,
+    minDate: today,
+  });
+  flatpickr('#booking-time', {
+    noCalendar: true,
+    enableTime: true,
+    dateFormat: 'H:i',
+    time_24hr: false,
+    defaultHour: today.getHours(),
+  });
+};
+
+window.addEventListener('load', fp);
+
+/*
+document.querySelector('.ii').addEventListener('click', e => {
+  console.log(e);
+  document.querySelector('#booking-date').click();
+});*/
